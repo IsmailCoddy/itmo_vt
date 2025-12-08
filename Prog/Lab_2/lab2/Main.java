124611113e4a61176b79477b01458fced186d34e
Ismail <88140588+IsmailCoddy@users.noreply.github.com>
2025-12-09 01:40:31 +0300

    Update Main.java

--- Prog/Lab_2/lab2/Main.java
+++ Prog/Lab_2/lab2/Main.java
@@ -7,16 +7,16 @@ import move.*;
 public class Main {
     public static void main(String[] args) {
         Battle b = new Battle();
-        //Basculin p1 = new Basculin("Баск", 1);
-        //Rattata p2 = new Rattata("Рат", 1);
+        Basculin p1 = new Basculin("Баск", 1);
+        Rattata p2 = new Rattata("Рат", 1);
         Raticate p3 = new Raticate("Баск", 1);
-        //Bounsweet p4 = new Bounsweet("Баун", 1);
+        Bounsweet p4 = new Bounsweet("Баун", 1);
         Steenee p5 = new Steenee("Сти", 1);
         Tsareena p6 = new Tsareena("Царина", 1);
-        //b.addAlly(p1);
-        //b.addFoe(p2);
+        b.addAlly(p1);
+        b.addFoe(p2);
         b.addAlly(p3);
-        //b.addFoe(p4);
+        b.addFoe(p4);
         b.addAlly(p5);
         b.addFoe(p6);
         b.go();
